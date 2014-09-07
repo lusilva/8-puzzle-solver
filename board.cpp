30aea7dcec7ff1cd119c230702376a1554b26338
Lucas Silva <silval@rpi.edu>
2014-09-07 19:33:43 -0400

    reorganized code

--- board.cpp
+++ board.cpp
@@ -1,8 +1,10 @@
-#include "board.h"
+// Copyright 2014 Lucas Silva
+
+#include "headers/board.h"
 
 Board::destroy_board() {
-	for(int i = 0; i < 3; ++i) {
-		delete [] this->board_[i];
-	}
-	delete [] this->board_;
+    for ( int i = 0; i < 3; ++i ) {
+        delete [] this->board_[i];
+    }
+    delete [] this->board_;
 }
