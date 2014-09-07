5a92396f55a7506f01fcd6a3f40758660315deea
Lucas Silva <silval@rpi.edu>
2014-09-07 17:25:17 -0400

    added board.h plus test cases

---
+++ board.h
@@ -0,0 +1,29 @@
+#ifndef _board_h
+#define _board_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+/*
+	The board class stores the states of the board and provides a way to interact with 
+	the board by making moves.
+*/
+
+class Board {
+public:
+
+
+
+
+
+
+private:
+	// Representation of a board
+
+
+};
+
+
+
+
+#endif
