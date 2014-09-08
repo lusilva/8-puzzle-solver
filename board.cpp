dcf42c18dcfa11b3ef5a920ac5254c5de215d4f9
Lucas Silva <silval@rpi.edu>
2014-09-08 12:06:40 -0400

    improved comments

--- board.cpp
+++ board.cpp
@@ -1,10 +1,19 @@
-// Copyright 2014 Lucas Silva
+/////////////////////////////////////////////////
+// Copyright 2014 Lucas Silva                  //
+// The implementation file for the board class //
+/////////////////////////////////////////////////
 
 #include "headers/board.h"
 
-Board::destroy_board() {
-    for ( int i = 0; i < 3; ++i ) {
-        delete [] this->board_[i];
+/**
+ * Deallocates dynamic memory used to store the board.
+ * @private
+ */
+Board::destroy_board_() {
+    if (this->memory_allocated_) {
+        for ( int i = 0; i < 3; ++i ) {
+            delete [] this->board_[i];
+        }
+        delete [] this->board_;
     }
-    delete [] this->board_;
 }
--- headers/board.h
+++ headers/board.h
@@ -1,39 +1,52 @@
+/////////////////////////////////////////
+// Copyright 2014 Lucas Silva          //
+// The header file for the board class //
+/////////////////////////////////////////
+
 #ifndef _board_h
 #define _board_h
 
 #include <iostream>
 #include <string>
+
 /*
 	The board class stores the states of the board and provides a way to interact with 
 	the board by making moves.
 */
-
 class Board {
-public:
-	Board(const std::string &input) : input_string_(input) {}
-	~Board() {
-		this->destroy_board();
-		this->board = NULL;
-	}
+ public:
+ 	/*
+ 	 * Constructor for the Board class. 
+ 	 * @param {string} input The user input
+ 	 */
+    explicit Board(const std::string &input) : input_string_(input) {}
+
+    /*
+     * Destructor for the Board class. Deallocates dyanmically created memory.
+     */
+    ~Board() {
+        this->destroy_board();
+        this->board = NULL;
+    }
 
 
 
+ private:
+    /* The input string given by the user. */
+    const std::string input_string_;
+    /* Flag to store when memory is allocated. */
+    bool memory_allocated_   = false;
+    /* Representation of a board as a dynamically allocated 2-D array */
+    int** board_[3][3]       = NULL;
+    /* Actual number of moves made so far */
+    int moves_made_          = 0;
+    /* Estimated moves remaining, using heuristic function. */
+    int est_moves_remaining_ = 0;
+    /* A pointer to the previous board state. */
+    Board* previous_state_   = NULL;
 
-
-
-private:
-     
-    /*   BOARD PROPERTIES   */
-	const std::string input_string_;  // The input string given by the user.
-	int** board_[3][3]       = NULL;  // Representation of a board.
-	int moves_made_          = 0; 	  // Actual number of moves made so far.
-	int est_moves_remaining_ = 0; 	  // Estimated moves remaining, using heuristic function.
-	Board* previous_state_   = NULL;  // A pointer to the previous board state.
-
-    /*   BOARD FUNCTIONS   */
-    // Free dynamically allocated memory for board.
-	void destroy_board();
-
+    /* Free dynamically allocated memory for board. */
+    void destroy_board();
 };
 
 
